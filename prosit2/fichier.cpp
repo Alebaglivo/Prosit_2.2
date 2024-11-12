963d7fa8f2fabb796785d15b052f14e1c4045819
Alebaglivo <126134786+Alebaglivo@users.noreply.github.com>
2024-11-12 10:25:44 +0100

    Update fichier.cpp

--- prosit2/fichier.cpp
+++ prosit2/fichier.cpp
@@ -1,1 +1,35 @@
-#include <fichier.h> 
+#include "fichier.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Constructeur optionnel, initialise le nom du fichier (si nécessaire)
+Fichier::Fichier() {
+    nomFichier = "C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt";
+}
+
+void Fichier::lecture() {
+    ifstream monFlux(nomFichier.c_str()); // Changement vers ifstream pour la lecture
+    if (monFlux) {
+        string ligne;
+        while (getline(monFlux, ligne)) {
+            cout << ligne << endl;
+        }
+    } else {
+        cerr << "ERREUR: Impossible d'ouvrir le fichier en mode lecture." << endl;
+    }
+}
+
+void Fichier::ecriture() {
+    ofstream monFlux(nomFichier.c_str());
+    if (monFlux) {
+        monFlux << "Bonjour, je suis une phrase écrite dans un fichier." << endl;
+        monFlux << 42.1337 << endl;
+        int age(36);
+        monFlux << "J'ai " << age << " ans." << endl;
+    } else {
+        cerr << "ERREUR: Impossible d'ouvrir le fichier en mode écriture." << endl;
+    }
+}
